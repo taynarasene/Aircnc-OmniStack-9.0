a631f30faf037155dde56fb85871981e1a30e190
taynara <taynara@pop-os.localdomain>
2019-10-02 00:48:50 -0300

    start :tada

---
+++ readme.md
@@ -0,0 +1,28 @@
+## Semana Omnisteck
+
+### API Methods
+
+GET, POST, PUT, DELETE
+
+* req.query = Acessar query params (Para Filtros)
+
+```
+/users/?idade=25
+```
+* req.params = Acessar route params (Para Edição e Delete)
+
+```
+/users/1
+```
+* req.body = Acessar o corpo da requisição (Para Criação)
+
+```
+{
+    "Idade" : 25
+}
+```
+
+### Mongo DB Atlas
+
+https://www.mongodb.com/cloud/atlas
+
---
+++ src/routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const multer = require('multer');
+
+const uploadConfig = require('./config/upload');
+
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+const DashboardContreller = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController')
+
+
+const routes = express.Router();
+const upload = multer(uploadConfig);
+
+
+routes.post('/sessions', SessionController.store);
+
+routes.get('/spots', SpotController.index)
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+
+routes.get('/dashboard', DashboardContreller.show);
+
+routes.post('spots/:spot_id/bookings', BookingController.store );
+
+
+module.exports = routes;
